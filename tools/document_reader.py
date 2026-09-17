a25c43069f9c23ba8dbc3f97edf684b24da35c93
Shon Santhosh <shonsanthosh00@gmail.com>
2026-09-17 20:50:25 +0530

    document fetching tool -- completed

--- tools/document_reader.py
+++ tools/document_reader.py
@@ -1,35 +1,117 @@
 from pathlib import Path
-import json
+import logging
 
+#filelib
+import json
+import csv
+import pymupdf
+from docx import Document
+
+# text file (.txt) reader
 def txt_reader(file_path):
     path = Path(file_path)
     if path.exists():
         try:
-            if path.suffix.lower() == ".txt":
+            if path.is_file() and path.suffix.lower() == ".txt":
                 content = path.read_text(encoding="utf-8")
                 return content
             else:
                 return "provide .txt file"
-        except Exception:
+        except Exception as err:
+            logging.error(f"ERROR: error occured due to {err}")
             return "error occured"
     if not path.exists():
         return "Path does not exist!"
 
-txt_reader("../documents/notes.txt")
-
+# markdown file (.md) reader
 def md_reader(file_path):
     path = Path(file_path)
     if path.exists():
         try:
-            if path.suffix.lower() == ".md":
+            if path.is_file() and path.suffix.lower() == ".md":
                 content = path.read_text(encoding="utf-8")
                 return content
             else:
                 return "provide .md file"
-        except Exception:
+        except Exception as err:
+                logging.error(f"ERROR: error occured due to {err}")
                 return "error occured"
     if not path.exists():
         return "Path does not exist!"
 
-md_reader("../documents/robotics.md")
+# json file (.json) reader
+def json_reader(file_path):
+    path = Path(file_path)
+    if path.exists():
+        try:
+            if path.is_file() and path.suffix.lower() == ".json":
+                with open(path, "r", encoding="utf-8") as file:
+                    content = json.load(file)
+                    return content
+            else:
+                return "provide .json file"
+        except Exception as err:
+            logging.error(f"ERROR: error occured due to {err}")
+            return "error occured"
+    if not path.exists():
+        return "Path doesn't exist"
 
+# csv file (.csv) reader
+def csv_reader(file_path):
+    path = Path(file_path)
+    if path.exists():
+        try:
+            if path.is_file() and path.suffix.lower() == ".csv":
+                with path.open("r", encoding="utf-8") as file:
+                    content = list(csv.DictReader(file))
+                    return content
+            else:
+                return "PROVIDE .CSV FILE!"
+        except Exception as err:
+            logging.error(f"ERROR: {err}")
+            return "Error occured"
+    if not path.exists():
+        return "PATH DOES NOT EXIST!"
+
+# pdf file (.pdf) reader
+def pdf_reader(file_path):
+    path = Path(file_path)
+    if path.exists():
+        try:
+            if path.is_file() and path.suffix.lower() == ".pdf":
+                content = pymupdf.open(file_path)
+                page_text = []
+                for page in content:
+                    text = page.get_text()
+                    page_text.append(text)
+
+                #CLOSE PDF DOCUMENT
+                content.close()
+                return page_text
+            else:
+                return "provide .pdf file!!"
+        except Exception as err:
+            logging.error(f"ERROR : {err}")
+            return "Error occured"
+    if not path.exists():
+        return "PATH DOESN'T EXIST!!"
+# docx file (.docx) reader
+def docx_reader(file_path):
+    path = Path(file_path)
+    if path.exists():
+        try:
+            if path.is_file() and path.suffix.lower() == ".docx":
+                content = Document(path)
+                content = content.paragraphs
+                content_text = []
+                for paragraph in content:
+                    text = paragraph.text
+                    content_text.append(text)
+                return content_text
+            else:
+                return "provide .docx file!!"
+        except Exception as err:
+            logging.error(f"ERROR : {err}")
+            return "Error occured"
+    if not path.exists():
+        return "PATH DOESN'T EXIST!!"
